82d525bd5f943f8ecaba22ef19ee2889bdeae734
Leonardo Conde Aoun <100950151+LeoAoun@users.noreply.github.com>
2023-04-10 19:48:28 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -17,26 +17,27 @@ document.querySelector(".container-weather").style.display = "none"
 document.querySelector("footer").style.display = "none"
 
 
-
 function Search() {
     let CITY = input.value
     let API_WEATHER = `https://api.openweathermap.org/data/2.5/weather?q=${CITY}&appid=${KEY_WEATHER}&units=${UNITS}&lang=pt_br`;
 
     fetch(API_WEATHER)
         .then(res => res.json())
-        .then(data => {
+        .then(data_weather => {
 
-            country.setAttribute("src", `https://flagsapi.com/${data.sys.country}/flat/48.png`)
+            let API_UNSPLASH = `https://api.unsplash.com/search/photos?query=${CITY}city,${data_weather.sys.country}&collections=cityscape`
 
-            fetch(`https://api.unsplash.com/search/photos?query=${CITY}&collections=cityscape`, {
+            country.setAttribute("src", `https://flagsapi.com/${data_weather.sys.country}/flat/48.png`)
+
+            fetch(API_UNSPLASH, {
                 headers: {
                     Authorization: `Client-ID ${KEY_UNSPLASH}`
                 }
             })
                 .then(res => res.json())
-                .then(data => {
-                    const randomIndex = Math.floor(Math.random() * data.results.length);
-                    const imageUrl = data.results[randomIndex].urls.regular;
+                .then(data_unsplash => {
+                    const randomIndex = Math.floor(Math.random() * data_unsplash.results.length);
+                    const imageUrl = data_unsplash.results[randomIndex].urls.regular;
 
                     const body = document.querySelector('body');
                     body.style.backgroundImage = `url('${imageUrl}')`
@@ -44,34 +45,33 @@ function Search() {
                     body.style.backgroundSize = "cover"
                     body.style.backgroundRepeat = "no-repeat"
                 })
-                .catch(error => {
-
-                });
 
             setTimeout(() => {
                 document.querySelector(".container-city").style.display = "flex"
                 document.querySelector(".container-weather").style.display = "flex"
                 document.querySelector("footer").style.display = "flex"
 
-                city.textContent = data.name
-                temperature.textContent = (data.main.temp).toFixed(1) + " °C"
+                city.textContent = data_weather.name
+                temperature.textContent = (data_weather.main.temp).toFixed(1) + " °C"
 
-                let description = (data.weather[0].description).split(" ")
+                let description = (data_weather.weather[0].description).split(" ")
                 for (let i = 0; i < description.length; i++) {
                     description[i] = description[i].charAt(0).toUpperCase() + description[i].slice(1)
                 }
                 weather.textContent = description.join(" ")
-                icon_weather.setAttribute("src", `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`)
+                icon_weather.setAttribute("src", `https://openweathermap.org/img/wn/${data_weather.weather[0].icon}@2x.png`)
 
-                humidity.textContent = data.main.humidity + "%"
-                wind.textContent = data.wind.speed + " km/h"
+                humidity.textContent = data_weather.main.humidity + "%"
+                wind.textContent = data_weather.wind.speed + " km/h"
 
             }, 500)
 
         })
         .catch(error => {
-            throw new Error()
+            
         })
+
+
 
 
 }
